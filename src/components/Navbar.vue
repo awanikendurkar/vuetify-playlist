f66a32efb16388962a50ee2c462c294be6d3c38e
awani <awanikendurkar@gmail.com>
2022-01-22 11:46:14 +0530

    Completed lesson 20 (Avatars)

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav>
+    <v-app-bar flat app color="white">
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-app-bar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed color="white grey--text">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <v-row class="text-center">
+        <v-col class="mt-5">
+          <v-avatar size="100">
+            <img src="/avatar-1.png" alt="" />
+          </v-avatar>
+          <p class="white--text subtitle-1 mt-1">The Net Ninja</p>
+        </v-col>
+      </v-row>
+      <v-list>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
+          <v-list-item-action>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{
+              link.text
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" },
+      ],
+    };
+  },
+};
+</script>
