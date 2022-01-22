ac84448d7b445c65a85ad2843626b6b52b850c04
awani <awanikendurkar@gmail.com>
2022-01-22 18:51:49 +0530

    Completed lesson 27 (Firestore Databases) // trying to setup firestore

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -10,20 +10,22 @@
         <h2>Add a new project</h2>
       </v-card-title>
       <v-card-text>
-        <v-form class="px-3">
+        <v-form class="px-3" ref="form">
           <v-text-field
             label="Title"
             v-model="title"
             prepend-icon="mdi-folder"
+            :rules="inputRules"
           ></v-text-field>
           <v-textarea
             label="Information"
             v-model="content"
             prepend-icon="mdi-pencil"
+            :rules="inputRules"
           ></v-textarea>
 
           <v-menu
-            v-model="menu2"
+            v-model="menu"
             :close-on-content-click="false"
             max-width="290"
           >
@@ -37,34 +39,14 @@
                 v-bind="attrs"
                 v-on="on"
                 @click:clear="date = null"
+                :rules="inputRules"
               ></v-text-field>
             </template>
             <v-date-picker
               v-model="date"
-              @change="menu2 = false"
+              @change="menu = false"
             ></v-date-picker>
           </v-menu>
-
-          <!-- <v-menu
-            v-model="menu"
-            :close-on-content-click="false"
-            max-width="290"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                :value="formattedDate"
-                clearable
-                label="Due date"
-                readonly
-                prepend-icon="mdi-calendar-range"
-                v-bind="attrs"
-                v-on="on"
-                @click:clear="due = null"
-              >
-              </v-text-field>
-            </template>
-            <v-date-picker v-model="due" @change="menu = false"></v-date-picker>
-          </v-menu> -->
 
           <v-spacer></v-spacer>
 
@@ -84,22 +66,19 @@ export default {
     return {
       title: "",
       content: "",
-      //   due: null, //format(parseISO(new Date().toISOString()), "yyyy-MM-dd"),
-      //   menu: false,
-      date: format(parseISO(new Date().toISOString()), "yyyy-MM-dd"),
-      menu2: false,
+      date: null,
+      menu: false,
+      inputRules: [(v) => v.length >= 3 || "Minimum length is 3 characters"],
     };
   },
   methods: {
     submit() {
-      console.log(this.title, this.content);
+      if (this.$refs.form.validate()) {
+        console.log("Added to db");
+      }
     },
   },
   computed: {
-    // formattedDate() {
-    //   console.log(this.due);
-    //   return this.due ? format(this.due, "Do MMM YYYY") : "";
-    // },
     computedDateFormattedDatefns() {
       return this.date ? format(parseISO(this.date), "do MMM yyyy") : "";
     },
