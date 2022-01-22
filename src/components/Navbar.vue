69a23280b2add092cd605330d476fa26af5bec60
awani <awanikendurkar@gmail.com>
2022-01-22 12:27:23 +0530

    Completed lesson 22 (Menus)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,6 +10,28 @@
         <span>Ninja</span>
       </v-app-bar-title>
       <v-spacer></v-spacer>
+
+      <!-- DROPDOWN MENU -->
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text color="grey" v-bind="attrs" v-on="on">
+            <v-icon left>mdi-chevron-down</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+          >
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn depressed color="white grey--text">
         <span>Sign Out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
